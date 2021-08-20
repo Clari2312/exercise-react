3e6fe6ec50a0250dc425f20822badba9435d8c81
raymondChris <ray_chris@hotmail.it>
2021-08-20 11:50:13 +0200

    finish to convert into functional component Card Component

--- src/components/List/Card/Card.js
+++ src/components/List/Card/Card.js
@@ -1,37 +1,74 @@
 import { Component } from "react";
 import "./Card.css";
 
-class Card extends Component {
-  render() {
-    console.log("props card: ", this.props);
-    const { userName, userLastName, status } = this.props.infoUser;
-    const { positionIndex, editEvent } = this.props;
-    return (
-      <div className="card">
-        <div className="card-info">
-          <p>{userName}</p>
-          <p>{userLastName}</p>
-          <div className={status ? "card-info_on" : "card-info_off"}></div>
-        </div>
-        <div className="card-actions">
-          <button
-            onClick={() =>
-              editEvent(userName, userLastName, status, positionIndex)
-            }
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => {
-              this.props.removeUser(positionIndex);
-            }}
-          >
-            Remove
-          </button>
-        </div>
+// class Card extends Component {
+//   render() {
+//     // console.log("props card: ", this.props);
+//     const { userName, userLastName, status } = this.props.infoUser;
+//     const { positionIndex, editEvent, removeUser } = this.props;
+//     return (
+//       <div className="card">
+//         <div className="card-info">
+//           <p>{userName}</p>
+//           <p>{userLastName}</p>
+//           <div className={status ? "card-info_on" : "card-info_off"}></div>
+//         </div>
+//         <div className="card-actions">
+//           <button
+//             onClick={() =>
+//               editEvent(userName, userLastName, status, positionIndex)
+//             }
+//           >
+//             Edit
+//           </button>
+//           <button
+//             onClick={() => {
+//               removeUser(positionIndex);
+//             }}
+//           >
+//             Remove
+//           </button>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Card;
+
+// function Card() { }
+const Card = (props) => {
+  // class Card extends Component {
+  // render() {
+  // console.log("props card: ", this.props);
+  const { userName, userLastName, status } = props.infoUser;
+  const { positionIndex, editEvent, removeUser } = props;
+  return (
+    <div className="card">
+      <div className="card-info">
+        <p>{userName}</p>
+        <p>{userLastName}</p>
+        <div className={status ? "card-info_on" : "card-info_off"}></div>
       </div>
-    );
-  }
-}
+      <div className="card-actions">
+        <button
+          onClick={() =>
+            editEvent(userName, userLastName, status, positionIndex)
+          }
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => {
+            removeUser(positionIndex);
+          }}
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
+  // }
+};
 
 export default Card;
