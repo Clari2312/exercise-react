f4aa2efa4e4fdf9576c57f9599a2937f0e2c2fde
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 12:14:08 +0200

    converted App.js to a functional component

--- src/App.js
+++ src/App.js
@@ -1,76 +1,78 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Component } from "react";
+import { Component, useState } from "react";
 import { FormComponent, ListComponent } from "./components";
 
-class App extends Component {
+// class App extends Component
+const App = (props) => {
   //function App() {
-  state = {
-    userList: [
-      {
-        userName: "Clarissa",
-        userLastName: "Mascalchi",
-        status: true,
-      },
-    ],
-  };
+  // state = {
+  //   userList: [
+  //     {
+  //       userName: "Clarissa",
+  //       userLastName: "Mascalchi",
+  //       status: true,
+  //     },
+  //   ],
+  // };
+  const [userList, setUserList] = useState([
+    {
+      userName: "Clarissa",
+      userLastName: "Mascalchi",
+      status: true,
+    },
+  ]);
 
-  removeUserHandler = (index) => {
+  const removeUserHandler = (index) => {
     console.log("Remove user...");
-    this.setState((prevState, prevProps) => {
-      const newUserList = [...prevState.userList];
+    setUserList((prevState, prevProps) => {
+      const newUserList = [...prevState];
       newUserList.splice(index, 1);
-      return {
-        userList: newUserList,
-      };
+      return [...newUserList];
     });
   };
 
   // addUserHandler = (formValues, status) => {
-  addUserHandler = ({ firstName, lastName }, status) => {
+  const addUserHandler = ({ firstName, lastName }, status) => {
     console.log("adding user...");
     //const { firstName, lastName } = formValues;
-    this.setState((prevState, prevProps) => {
-      const newList = [...prevState.userList];
+    setUserList((prevState, prevProps) => {
+      const newList = [...prevState];
       newList.push({
         userName: firstName,
         userLastName: lastName,
         status: status,
       });
 
-      return {
-        userList: newList,
-      };
+      return [...newList];
     });
   };
 
-  editUserHandler = (newUserInfo, status, userIndex) => {
-    console.log("newUserInfo ", newUserInfo);
-    console.log("status ", status);
-    console.log("userIndex ", userIndex);
-    let newList = [...this.state.userList];
+  const editUserHandler = (newUserInfo, status, userIndex) => {
+    // console.log("newUserInfo ", newUserInfo);
+    // console.log("status ", status);
+    // console.log("userIndex ", userIndex);
+    let newList = [...userList];
     newList[userIndex] = {
       userName: newUserInfo.firstName,
       userLastName: newUserInfo.lastName,
       status: status,
     };
-    this.setState({
-      userList: newList,
-    });
+    setUserList([...newList]);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <FormComponent addUser={this.addUserHandler} />
-        <ListComponent
-          editUser={this.editUserHandler}
-          infoUsers={this.state.userList}
-          removeUser={this.removeUserHandler}
-        />
-      </div>
-    );
-  }
-}
+  // render() {
+  return (
+    <div className="App">
+      <FormComponent addUser={addUserHandler} />
+      <ListComponent
+        editUser={editUserHandler}
+        infoUsers={userList}
+        removeUser={removeUserHandler}
+      />
+    </div>
+  );
+  // }
+};
 
 export default App;
