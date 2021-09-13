efe9214ef23f070e09c886bdf12baf21c5c542ce
cmascalchidema <c.mascalchi@dema-engineering.it>
2021-09-13 12:35:08 +0200

    converted file Form.js to a functional component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,141 +1,141 @@
-import { Component } from "react";
+import { Component, useState } from "react";
 import "./Form.css";
 
-class Form extends Component {
-  state = {
-    form: {
-      firstName: "",
-      lastName: "",
-    },
-  };
+// class Form extends Component
+const Form = (props) => {
+  // state = {
+  //   form: {
+  //     firstName: "",
+  //     lastName: "",
+  //   },
+  // };
 
-  componentDidMount() {
-    console.log("pippo ", this.props);
-    if (this.props.inEdit) {
-      this.setState({
-        form: {
-          firstName: this.props.userInfo.userName,
-          lastName: this.props.userInfo.userLastName,
-        },
-      });
-    }
-  }
+  const [formState, setFormState] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-  resetInputValueHandler = () => {
+  // componentDidMount() {
+  //   console.log("pippo ", this.props);
+  //   if (this.props.inEdit) {
+  //     this.setState({
+  //       form: {
+  //         firstName: this.props.userInfo.userName,
+  //         lastName: this.props.userInfo.userLastName,
+  //       },
+  //     });
+  //   }
+  // }
+
+  const resetInputValueHandler = () => {
     const inputName = document.getElementById("form-input_name");
     const inputLastName = document.getElementById("form-input_lastname");
     inputName.value = "";
     inputLastName.value = "";
   };
 
-  inputFirstNameHandler = (event) => {
+  // inputFirstNameHandler = (event) => {
+  const inputFirstNameHandler = (event) => {
     // console.log("event: ", event.target.value);
-    this.setState((prevState, prevProps) => {
+    setFormState((prevState, prevProps) => {
       return {
-        form: {
-          firstName: event.target.value,
-          lastName: prevState.form.lastName,
-        },
+        firstName: event.target.value,
+        lastName: prevState.lastName,
       };
     });
   };
 
-  inputLastNameHandler = (event) => {
+  // inputLastNameHandler (event) {
+  const inputLastNameHandler = (event) => {
     // console.log("event: ", event.target.value);
-    this.setState((prevState, prevProps) => {
+    setFormState((prevState, prevProps) => {
       return {
-        form: {
-          firstName: prevState.form.firstName,
-          lastName: event.target.value,
-        },
+        firstName: prevState.firstName,
+        lastName: event.target.value,
       };
     });
   };
 
-  render() {
-    console.log("form props: ", this.props);
-    const { disableEvent, inEdit, userInfo, editUser } = this.props;
+  // render() {
+  console.log("form props: ", props);
+  const { disableEvent, inEdit, userInfo, editUser } = props;
 
-    return (
-      <div className="form" style={{ width: inEdit ? "100%" : "50%" }}>
-        <form>
-          <div className="form-input">
-            <label>Name:</label>
-            <input
-              id="form-input_name"
-              type="text"
-              onChange={(e) => this.inputFirstNameHandler(e)}
-              defaultValue={inEdit ? userInfo.userName : ""}
-            ></input>
-          </div>
-          <div className="form-input">
-            <label>LastName:</label>
-            <input
-              id="form-input_lastname"
-              type="text"
-              onChange={(e) => this.inputLastNameHandler(e)}
-              defaultValue={inEdit ? userInfo.userLastName : ""}
-            ></input>
-          </div>
-          <div className="form-actions">
+  return (
+    <div className="form" style={{ width: inEdit ? "100%" : "50%" }}>
+      <form>
+        <div className="form-input">
+          <label>Name:</label>
+          <input
+            id="form-input_name"
+            type="text"
+            onChange={(e) => inputFirstNameHandler(e)}
+            defaultValue={inEdit ? userInfo.userName : ""}
+          ></input>
+        </div>
+        <div className="form-input">
+          <label>LastName:</label>
+          <input
+            id="form-input_lastname"
+            type="text"
+            onChange={(e) => inputLastNameHandler(e)}
+            defaultValue={inEdit ? userInfo.userLastName : ""}
+          ></input>
+        </div>
+        <div className="form-actions">
+          <button
+            className="form-actions_off"
+            onClick={(e) => {
+              e.preventDefault();
+
+              if (inEdit) {
+                editUser(formState, false, userInfo.index);
+                disableEvent();
+              } else {
+                setFormState({
+                  firstName: "",
+                  lastName: "",
+                });
+                resetInputValueHandler();
+                props.addUser(formState, false);
+              }
+            }}
+          >
+            Offline
+          </button>
+          <button
+            className="form-actions_on"
+            onClick={(e) => {
+              e.preventDefault();
+              if (inEdit) {
+                editUser(formState, true, userInfo.index);
+                disableEvent();
+              } else {
+                setFormState({
+                  firstName: "",
+                  lastName: "",
+                });
+                resetInputValueHandler();
+                props.addUser(formState, true);
+              }
+            }}
+          >
+            Online
+          </button>
+          {inEdit ? (
             <button
-              className="form-actions_off"
               onClick={(e) => {
                 e.preventDefault();
-
-                if (inEdit) {
-                  editUser(this.state.form, false, userInfo.index);
-                  disableEvent();
-                } else {
-                  this.setState({
-                    form: {
-                      firstName: "",
-                      lastName: "",
-                    },
-                  });
-                  this.resetInputValueHandler();
-                  this.props.addUser(this.state.form, false);
-                }
+                disableEvent();
               }}
             >
-              Offline
+              Cancel
             </button>
-            <button
-              className="form-actions_on"
-              onClick={(e) => {
-                e.preventDefault();
-                if (inEdit) {
-                  editUser(this.state.form, true, userInfo.index);
-                  disableEvent();
-                } else {
-                  this.setState({
-                    form: {
-                      firstName: "",
-                      lastName: "",
-                    },
-                  });
-                  this.resetInputValueHandler();
-                  this.props.addUser(this.state.form, true);
-                }
-              }}
-            >
-              Online
-            </button>
-            {inEdit ? (
-              <button
-                onClick={(e) => {
-                  e.preventDefault();
-                  disableEvent();
-                }}
-              >
-                Cancel
-              </button>
-            ) : null}
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+          ) : null}
+        </div>
+      </form>
+    </div>
+  );
+};
+// }
 
 export default Form;
